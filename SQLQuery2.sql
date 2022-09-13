e43491b715511c546261b5a79d50fad3589a2a47
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-09-13 11:52:48 +0530

    Retrive according to data

---
+++ SQLQuery2.sql
@@ -0,0 +1,24 @@
+-------To Create Database-------
+Create database Payroll_Service2
+
+
+-------To Create Table (UC2)----
+Create table Employee_PayRoll(
+Emp_ID int identity(1,1) PRIMARY KEY,----first 1=it should be start with first row and second indicate 1=increment by 1---
+Name varchar(200),
+Salary float,
+StartDate datetime 
+);
+
+------To insert the data into Table----
+insert into Employee_PayRoll
+(Name,Salary,StartDate)values('Anand',30000,2022-07-20)
+
+------Retrieve data (UC4)-----
+select *from Employee_PayRoll
+
+------Retrieve data of particular empolyee(UC5)-----
+select *from Employee_PayRoll where Name='Anand'; 
+
+-----------RetriveAccordingtodate---------------
+SELECT * FROM Employee_PayRoll where StartDate between CAST('1905-06-19' as date) and GETDATE()
